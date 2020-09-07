80e913934797c90899256e99c8c65432536f3802
imanoj997 <mbhatt997@gmail.com>
2020-09-07 14:30:27 +0545

    field choices sorted

--- ranking/views.py
+++ ranking/views.py
@@ -8,14 +8,16 @@ from core.models import HouseHoldData, OwnerFamilyData, OtherFamilyMember, Anima
 def calculateScore(request, id):
     this_house = HouseHoldData.objects.filter(index=id)
     mappable_questions = Question.objects.filter(directly_mappable=True)
-    non_mappable_questions = Question.objects.filter(directly_mappable=False).values_list('map_to_model', 'map_to_field')
+    non_mappable_questions = Question.objects.filter(directly_mappable=False)
     for question in mappable_questions:
         final_score = 0
         level0_score = 0
         level1_score = 0
         level2_score = 0
         print(question, "==============")
-
+    # all_fields = HouseHoldData._meta.fields
+    all_fields = [f.name for f in HouseHoldData._meta.get_fields()]
+    print(all_fields)
     html = "<html><body><p>Ranking Households</p></body></html>"
     return HttpResponse(html)
 
