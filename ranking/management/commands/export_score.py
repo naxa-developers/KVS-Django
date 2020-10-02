dff4b39e50756c6cd05d6450fbfd1f4ea73247d8
imanoj997 <mbhatt997@gmail.com>
2020-10-02 10:35:41 +0545

    score optimization 10-2-1

---
+++ ranking/management/commands/export_score.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand, CommandError
+from celery import shared_task
+import csv
+
+from ranking.views import calculateHouseHoldScore
+from core.models import HouseHoldData
+from ranking.tasks import calcScoreFromCelery
+
+class Command(BaseCommand):
+    help = "Use this command to calculate the risk score of all the data of HouseHold model"
+
+    def handle(self, *args, **options):
+        all_household = HouseHoldData.objects.all()
+        for household in all_household:
+            f = open("score_list.csv", 'a')
+            outfileWriter = csv.writer(f, delimiter=',')
+            outfileWriter.writerow([household.id, household.owner_name, household.risk_score])
+            f.close()
--- ranking/utils.py
+++ ranking/utils.py
@@ -1,13 +1,15 @@
 from collections import Iterable
 import re
 
+
 def flatten(lis):
-     for item in lis:
-         if isinstance(item, Iterable) and not isinstance(item, str):
-             for x in flatten(item):
-                 yield x
-         else:
-             yield item
+    for item in lis:
+        if isinstance(item, Iterable) and not isinstance(item, str):
+            for x in flatten(item):
+                yield x
+        else:
+            yield item
+
 
 def returnNum(current_answer, comp_type):
     numbers = []
@@ -28,19 +30,21 @@ def returnNum(current_answer, comp_type):
                         numbers.append(int(float(word1)))
                 elif '0.' in word1:
                     if float(word1) not in numbers:
-                        numbers.append((float(word1))) 
+                        numbers.append((float(word1)))
     return numbers
+
 
 def returnRiskType(risk_score):
     if risk_score < 30:
         risk_type = 'Least Vulnerable'
-    elif risk_score >=30 and risk_score <=70:
+    elif risk_score >= 30 and risk_score <= 70:
         risk_type = 'Medium Vulnerable'
     elif risk_score > 70:
         risk_type = 'Highly Vulnerable'
     else:
         risk_type = 'Medium Vulnerable'
     return risk_type
+
 
 def splitIntStr(string):
     if string.strip().isalnum():
@@ -52,11 +56,30 @@ def splitIntStr(string):
             value_str = ''
             for st in values:
                 value_str = value_str + st
-            this_num = int(float(value_str))
+            this_num = float(value_str)
             values = [s for s in string if s.isalpha()]
             this_string = ''
             for st in values:
                 this_string = this_string + st
+    elif '.' in string.strip():
+        if ' ' in string.strip():
+            striped_string = [s for s in string.split(" ") if s != '']
+            for st in striped_string:
+                if st.isnumeric():
+                    this_num = float(st)
+                elif st.replace('.', '', 1).isdigit():
+                    this_num = float(st)
+                elif st.isalpha():
+                    this_string = st
+                else:
+                    this_num = 0
+                    this_string = st
+        else:
+            all_chars = [s for s in string]
+            this_num = ''.join(e for e in all_chars if not str(e).isalpha())
+            this_num = float(this_num)
+            this_string = ''.join(str(e)
+                                  for e in all_chars if str(e).isalpha())
     elif ' ' in string.strip():
         striped_string = [s for s in string.split(" ") if s != '']
         for st in striped_string:
